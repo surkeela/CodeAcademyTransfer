c4b59d8c4d01553ba8f0e1a95b87a70295a108e7
Nerijus Šurkys <surkeela@gmail.com>
2023-07-18 18:38:05 +0300

    Initial Commit

---
+++ CodeAcademyTransfer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeAcademyTransfer
+//
+//  Created by Nerijus Surkys on 2023-07-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
